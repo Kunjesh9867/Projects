a0f91622a950c7b51f2019f691e7089c96d734e5
Kunjesh Ramani <kunjeshramani9867@gmail.com>
2023-03-14 16:04:47 -0400

    BlackJack README file added

--- C-Sharp/BlackJack/BlackJack/BlackJack.cs
+++ C-Sharp/BlackJack/BlackJack/BlackJack.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using Microsoft.VisualBasic.CompilerServices;
+
 //Here the laptop is dealer and the player is user, I have used this reference to make it simple
 // BlackJack Reference Video = https://youtu.be/eyoh-Ku9TCI
 
@@ -9,20 +11,40 @@ public class BlackJack
     // This is the inroduction of the Casino where the return value is the money that the player have to bet 
     public static double introductionTOGame()
     {
-        Console.WriteLine("**********");
+        for (int i = 0; i < 40; i++)
+        {
+            Console.Write("-");
+        }
+        Console.WriteLine();
         Console.WriteLine("Welcome to the BlackJack Game :)");
         Console.WriteLine("Welcome to Phoenix Casino!!!");
-        Console.WriteLine("**********");
+        for (int i = 0; i < 40; i++)
+        {
+            Console.Write("-");
+        }
+        Console.WriteLine();
 
         Console.Write("How much money would you like to bet today?  ");
-        return Convert.ToDouble(Console.ReadLine());
+        double d = Convert.ToDouble(Console.ReadLine());
+        for (int i = 0; i < 40; i++)
+        {
+            Console.Write("-");
+        }
+        Console.WriteLine();
+        return d;
     }
 
     // This is the method that ask the player whether he/she have to continue or not 
     public static int askToContinue()
     {
-        Console.WriteLine("Would you like to play? (true(1) or false(0)): ");
-        return Convert.ToInt16(Console.ReadLine());
+        Console.Write("Would you like to play? (Yes[1] or No[0]: ");
+        int integer =  Convert.ToInt16(Console.ReadLine());
+        for (int j = 0; j < 40; j++)
+        {
+            Console.Write("-");
+        }
+        Console.WriteLine();
+        return integer;
     }
 
     //This is the main function in which the game start to initiate
@@ -51,8 +73,13 @@ public class BlackJack
                 Random random = new Random(); // to get the random value for the card
                 
                 Console.WriteLine("The bet for this round is:"+betPrice[i]);
-                Console.WriteLine("Would You Like To Continue(y/n)? ");
+                Console.Write("Would You Like To Continue(y/n)? ");
                 String userInput = Console.ReadLine();
+                for (int j = 0; j < 40; j++)
+                {
+                    Console.Write("-");
+                }
+                Console.WriteLine();
                 if (userInput == "y")
                 {
                     int randomIndex2 = random.Next(0, length); // to get the first random value of the card for laptop
@@ -68,13 +95,18 @@ public class BlackJack
 
                     {
                         Console.WriteLine("You can convert your ace in 1 or 11, as you wish");
-                        Console.WriteLine("How do you want to convert Your Ace(1 or 11): ");
+                        Console.Write("How do you want to convert Your Ace(1 or 11): ");
                         String input = Console.ReadLine();
                         myNum[randomIndex] = int.Parse(input);
                     }
                     
                     var randomIndex3 = random.Next(0, length); // to get the  random value of the card for laptop
                     Console.WriteLine("Your second card is: "+myNum[randomIndex3]);
+                    for (int j = 0; j < 40; j++)
+                    {
+                        Console.Write("-");
+                    }
+                    Console.WriteLine();
                     if (myNum[randomIndex3] == "Jack" || myNum[randomIndex3] == "Queen" || myNum[randomIndex3] == "King")
                     {
                         randomIndex = 10;
@@ -101,7 +133,7 @@ public class BlackJack
                     while (newCard)
                     {
                         //To ask the player if he/she want another card or not
-                        Console.WriteLine("Do You want another card from the deck? Presss 1=Hit or 2=Stay: ");
+                        Console.Write("Do You want another card from the deck? Presss 1=Hit or 2=Stay: ");
                         String anotherCard = Console.ReadLine();
                         if (int.Parse(anotherCard) == 1) //when the player wants the card
                         {
@@ -113,7 +145,7 @@ public class BlackJack
                             }else if (myNum[randomIndex3] == "Ace")
                             {
                                 Console.WriteLine("You can convert your Ace in 1 or 11, as you wish");
-                                Console.WriteLine("How do you want to convert Your Ace(1 or 11): ");
+                                Console.Write("How do you want to convert Your Ace(1 or 11): ");
                                 String input = Console.ReadLine();
                                 myNum[randomIndex3] = int.Parse(input);
                             }
@@ -122,6 +154,11 @@ public class BlackJack
                             // if the total is greater than 21 for the player then the player is busted and he/she losses the money
                             if (total > 21)
                             {
+                                for (int j = 0; j < 40; j++)
+                                {
+                                    Console.Write("-");
+                                }
+                                Console.WriteLine();
                                 Console.WriteLine("You are Busted!!!");
                                 moneyToBet -= betPrice[i];
                                 break;
@@ -130,6 +167,11 @@ public class BlackJack
                             
                         }else if (int.Parse(anotherCard) == 2) // When the player don't want the card
                         {
+                            for (int j = 0; j < 40; j++)
+                            {
+                                Console.Write("-");
+                            }
+                            Console.WriteLine();
                             var randomIndex5 = random.Next(0, length);  // to get the random card
                             Console.WriteLine("Laptop's second card is: "+myNum[randomIndex5]); 
                             
@@ -156,6 +198,11 @@ public class BlackJack
                             // If the total card value for the laptop is >21, then the laptop losses and the player get 2x
                             if (Convert.ToInt16(myNum[randomIndex2]) + Convert.ToInt16(myNum[randomIndex5]) > 21)
                             {
+                                for (int j = 0; j < 40; j++)
+                                {
+                                    Console.Write("-");
+                                }
+                                Console.WriteLine();
                                 Console.WriteLine("Dealer is Busted!!!");
                                 Console.WriteLine("You won 2 times i.e: "+betPrice[i]*2);
                                 moneyToBet += betPrice[i] * 2 ;
@@ -165,7 +212,12 @@ public class BlackJack
 
                             else if (Convert.ToInt16(myNum[randomIndex2]) + Convert.ToInt16(myNum[randomIndex5]) < 16)
                             {
-                                Console.WriteLine("You have to take another card: ");
+                                for (int j = 0; j < 40; j++)
+                                {
+                                    Console.Write("-");
+                                }
+                                Console.WriteLine();
+                                Console.Write("You have to take another card: ");
                                 //Thread.Sleep(3000);
                                 randomIndex6 = random.Next(0, length);
                                 Console.WriteLine("Your card is: "+myNum[randomIndex6]);
@@ -184,7 +236,11 @@ public class BlackJack
                                 //if total of player >21, player losses 
                                 if (total > 21)
                                 {
-                                    
+                                    for (int j = 0; j < 40; j++)
+                                    {
+                                        Console.Write("-");
+                                    }
+                                    Console.WriteLine();
                                     Console.WriteLine("You are Busted!");
                                     moneyToBet -= betPrice[i];
                                     
@@ -193,6 +249,11 @@ public class BlackJack
                                 else if ((total > Convert.ToInt16(myNum[randomIndex2]) +
                                               Convert.ToInt16(myNum[randomIndex5])))
                                 {
+                                    for (int j = 0; j < 40; j++)
+                                    {
+                                        Console.Write("-");
+                                    }
+                                    Console.WriteLine();
                                     Console.WriteLine("Hurray you won 2 times:"+betPrice[i]*2);
                                     moneyToBet += betPrice[i] * 2;
                                 }
@@ -212,11 +273,21 @@ public class BlackJack
                                 if (Convert.ToInt16(myNum[randomIndex]) + Convert.ToInt16(myNum[randomIndex3]) +
                                     Convert.ToInt16(myNum[randomIndex4]) + Convert.ToInt16(myNum[randomIndex6]) > 16)
                                 {
+                                    for (int j = 0; j < 40; j++)
+                                    {
+                                        Console.Write("-");
+                                    }
+                                    Console.WriteLine();
                                     Console.WriteLine("You won 2 times of your bet: "+ betPrice[i]*2);
                                     moneyToBet += betPrice[i] * 2;
                                 }
                                 else
                                 {
+                                    for (int j = 0; j < 40; j++)
+                                    {
+                                        Console.Write("-");
+                                    }
+                                    Console.WriteLine();
                                     Console.WriteLine("You lose!");
                                     moneyToBet -= betPrice[i];
                                 }
@@ -227,6 +298,11 @@ public class BlackJack
                         //Just to check whether the loop works or not
                         else
                         {
+                            for (int j = 0; j < 40; j++)
+                            {
+                                Console.Write("-");
+                            }
+                            Console.WriteLine();
                             Console.WriteLine("Something went wrong.");
                             
                         }
@@ -245,6 +321,11 @@ public class BlackJack
 
         if (askToContinue() == 0)
         {
+            for (int j = 0; j < 40; j++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
             Console.WriteLine("Total Win: "+moneyToBet);
         }
         //Console.WriteLine("This bet is of $");
--- C-Sharp/BlackJack/README.md
+++ C-Sharp/BlackJack/README.md
@@ -1,5 +1,55 @@
-# Blackjack
+# BlackJack
 
 ## Players
-- [x] User
-- [x] Dealer
+- [x] Player (User)
+- [x] Laptop (Dealer)
+ 
+ ## Introduction
+
+ - In this Assignment, I have created a Console-based game between a player and laptop
+ - It is a simple Blackjack game.
+ - It comprises all the rules.
+ - The cards of the laptop are automated.
+ - It has a catchy design to easily go through the game.
+
+ ## Project features
+ - You can play this game in your leisure time ;)  
+  
+## Programming Language and Technologies used
+ 
+ - [x] C#
+ - [x] JetBrains Rider
+
+
+## Resources
+* College Notes
+* YouTube Tutorials
+* W3Schools
+* GeeksforGeeks
+* Stack Overflow
+
+
+## Website Screenshots
+
+### Game Screenshots
+
+1. Introduction 
+
+![Game-Introduction](./images/SS-1.png)
+
+<hr> 
+
+2. Main interface
+
+* Page-1
+
+![Page-1](./images/SS-2.png)
+
+<hr>
+
+* Page-2
+
+![Page-2](./images/SS-3.png)
+
+# Thank You
+
